aa3be657f488f468f0a0e74bb5d55f6a1064cf6c
javier argueta <r3tro0001@gmail.com>
2021-11-05 21:02:32 -0500

    Last update with online db, ready to run

--- src/main/java/xyz/opinionshop/Clases/OperacionesBD/BD.java
+++ src/main/java/xyz/opinionshop/Clases/OperacionesBD/BD.java
@@ -6,9 +6,9 @@ public class BD {
     protected static PreparedStatement Consulta;
     protected static ResultSet RS = null;
     private static final String base = "java";
-    private static final String user = "root";
-    private static final String password = "";
-    private static final String url = "jdbc:mysql://localhost:3306/" + base + "?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+    private static final String user = "admin";
+    private static final String password = "Thelifework01$";
+    private static final String url = "jdbc:mysql://portfolio.czgafo92x7sm.us-east-2.rds.amazonaws.com:3306/" + base + "?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
     
    public static Connection Conectar() throws SQLException, ClassNotFoundException {
       if (Conexion == null) {
